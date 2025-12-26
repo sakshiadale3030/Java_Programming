dbf73c856114bbdc210b8a8f161996e4eb7ee586
sakshiadale3030 <sakshiadale30@gmail.com>
2025-12-26 23:36:16 +0530

    PracticeWork

---
+++ PracticeWork/program14.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Number
+{
+   public boolean CheckPerfect(int iNo)
+   {
+      int i = 0;
+      int iSum = 0;
+
+      for(i = 1; i<= (iNo / 2); i++)
+      {
+        if ( (iNo % i) == 0)
+        {
+           iSum = iSum + 
+        }
+      }
+   }
+} // End of Number Class
+
+class program79
+{
+   public static void main(String A[])
+   {
+      int iValue = 0;
+      boolean bRet = false;
+     
+      Scanner sobj = new Scanner(System.in);
+
+      System.out.println("Enter number :");
+      iValue = sobj.nextInt();
+
+      Number nobj = new Number();
+      bRet = nobj.CheckPerfect(iValue);
+
+      if(bRet == true)
+      {
+         System.out.println(iValue+" is Perfect number");
+      }
+      else
+      {
+         System.out.println(iValue+" is not Perfect number");
+      }
+
+      //Important
+      sobj = null;
+      nobj = null;
+
+      System.gc();
+      
+   } 
+    
+}
