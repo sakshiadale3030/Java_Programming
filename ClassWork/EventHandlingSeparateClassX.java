a195f245973a0fcf843229fd2af2c65eb56f1cf7
sakshiadale3030 <sakshiadale30@gmail>
2026-01-13 12:47:47 +0530

    ClassWork

---
+++ ClassWork/EventHandlingSeparateClassX.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+class MarvellousListener implements ActionListener 
+{
+    public void actionPerformed(ActionEvent aobj)
+           {
+              System.out.println("Button clicked...");
+           }
+}
+class EventHandlingSeparateClassX
+{
+    public static void main(String A[])
+    {
+       JFrame fobj = new JFrame("PPA");
+       JButton bobj = new JButton("OK");
+
+       bobj.setBounds(100,100,150,50);
+       
+       MarvellousListener mobj = new MarvellousListener();
+       bobj.addActionListener(mobj);
+
+       fobj.add(bobj);
+
+       fobj.setSize(400,300);
+       fobj.setLayout(null);
+       fobj.setVisible(true);
+
+       fobj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
