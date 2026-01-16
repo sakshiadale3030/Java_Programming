cfc96faf485f02ad2f62724a7a3704314c201459
sakshiadale3030 <sakshiadale30@gmail>
2026-01-16 13:39:11 +0530

    ClassWork

---
+++ ClassWork/FinalMethod2.java
@@ -0,0 +1,25 @@
+class Base
+{
+    final public void fun()   //defination
+    {}
+
+    final public void gun()  //defination
+    {}
+}
+
+class Derived extends Base
+{
+    public void run()   //defination
+    {}
+    public void gun()   //override
+    {}
+}
+class FinalMethod2
+{
+    public static void main(String A[])
+    {
+       
+       
+
+    } 
+}
